e63dcb36297e580278342d048c053ad3f291e8bc
firebase database <monysalem365@gmail.com>
2023-07-12 20:20:56 +0300

    init

---
+++ lib/screens/fetch_data.dart
@@ -0,0 +1,98 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/material.dart';
+//import 'update_record.dart';
+
+class FetchData extends StatefulWidget {
+  const FetchData({Key? key}) : super(key: key);
+
+  @override
+  State<FetchData> createState() => _FetchDataState();
+}
+
+class _FetchDataState extends State<FetchData> {
+
+  Query dbRef = FirebaseDatabase.instance.ref().child('sensors');
+  DatabaseReference reference = FirebaseDatabase.instance.ref().child('sensors');
+
+  Widget listItem({required Map<String,dynamic> student}) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      height: 110,
+      color: Colors.amberAccent,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            student['Heart rate'],
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  //Navigator.push(context, MaterialPageRoute(builder: (_) => UpdateRecord(studentKey: student['key'])));
+                },
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.edit,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+              GestureDetector(
+                onTap: () {
+                  reference.child(student['key']).remove();
+                },
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.delete,
+                      color: Colors.red[700],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetching data'),
+        ),
+        body: Container(
+          height: double.infinity,
+          child: FirebaseAnimatedList(
+            query: dbRef,
+            itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index) {
+
+              Map<String,dynamic> student = snapshot.value as Map<String,dynamic>;
+              student['key'] = snapshot.key;
+
+              return listItem(student: student);
+
+            },
+          ),
+        )
+    );
+  }
+}
+
+
+
